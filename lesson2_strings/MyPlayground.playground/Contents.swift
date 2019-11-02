a1af6af6d0e56edd320f8bfe76feaa66d192bdfe
Winitei <winnie610@gmail.com>
2019-11-02 22:38:10 +0800

    Got Swift Playgrounds working in XCODE and practicing strings in sandbox.

---
+++ lesson2_strings/MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var welcome = "I love Kenneth Chang and I want to have his babies."
+
+let name = "Rosa"
+let personalizedGreeting = "Welcome, \(name)!"
+// personalizedGreeting == "Welcome, Rosa!"
+
+let price = 2
+let number = 3
+let cookiePrice = "\(number) cookies: $\(price * number)."
+// cookiePrice == "3 cookies: $6."
